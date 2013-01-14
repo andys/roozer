7f9a71628e59a9e1bd49da53c45416f628b952d8
Andrew Snow <andrew.snow@bigcommerce.com>
2013-01-14 18:56:12 +1100

    capistrano for deploying

--- Gemfile
+++ Gemfile
@@ -7,19 +7,7 @@ gem 'rails', '3.2.11'
 
 #gem 'sqlite3'
 
-
-# Gems used only for assets and not required
-# in production environments by default.
-#group :assets do
-#  gem 'sass-rails',   '~> 3.2.3'
-#  gem 'coffee-rails', '~> 3.2.1'
-#  gem 'uglifier', '>= 1.0.3'
-#end
-
 gem 'jquery-rails'
-
-# To use ActiveModel has_secure_password
-# gem 'bcrypt-ruby', '~> 3.0.0'
 
 # To use Jbuilder templates for JSON
 # gem 'jbuilder'
@@ -28,10 +16,7 @@ gem 'jquery-rails'
 # gem 'unicorn'
 
 # Deploy with Capistrano
-# gem 'capistrano'
-
-# To use debugger
-# gem 'debugger'
+gem 'capistrano'
 
 gem 'fraggle-block'
 gem 'thin'
---
+++ config/deploy.rb
@@ -0,0 +1,43 @@
+require "bundler/capistrano"
+
+set :deploy_via, :remote_cache
+set :application, "roozer"
+set :repository,  "git://github.com/andys/roozer.git"
+set :deploy_to, "/home/ubuntu/roozer"
+set :scm, :git
+role :app
+set :user, "ubuntu"
+set :use_sudo, false
+ssh_options[:keys] = [ENV["SSH_KEY"]] if ENV["SSH_KEY"]
+
+namespace :foreman do
+  desc "Export the Procfile to Ubuntu's upstart scripts"
+  task :export, :roles => :app do
+    run "cd #{deploy_to}/current && sudo bundle exec foreman export upstart /etc/init -u ubuntu"
+  end
+
+  desc "Start the application services"
+  task :start, :roles => :app do
+    sudo "start app"
+  end
+
+  desc "Stop the application services"
+  task :stop, :roles => :app do
+    sudo "stop app"
+  end
+
+  desc "Restart the application services"
+  task :restart, :roles => :app do
+    run "sudo restart app"
+  end
+end
+
+task :after_symlink do
+  run "ln -nfs #{shared_path}/servers.txt #{release_path}/db/servers.txt"
+#  run "ln -nfs #{shared_path}/doozer_server.crt #{release_path}/db/doozer_server.crt"
+#  run "ln -nfs #{shared_path}/doozer_server.key #{release_path}/db/doozer_server.key"
+end
+
+after "deploy:update", "foreman:export"
+after "deploy:restart", "deploy:cleanup"
+after "deploy:symlink", "after_symlink"
